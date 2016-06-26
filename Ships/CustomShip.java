378bbd817a95a09775ca3d2166a223e0910b45c6
Mateusz Parafinski <mateusz.parafinski@gmail.com>
2016-06-27 01:04:19 +0200

    Added CustomShip (mostly to enable one-square hover)

---
+++ Ships/CustomShip.java
@@ -0,0 +1,12 @@
+package Ships;
+
+public class CustomShip extends Ship{
+	
+	public CustomShip(int x){
+		super(x);
+	}
+	
+	public String toString(){
+		return "Custom Ship"; //TODO - change to something sneaky (JOptionPane?)
+	}
+}
